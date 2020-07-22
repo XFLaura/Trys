afef010748aff7dea8ae33206854459f8fc9663f
sophie.xi <sophie.xi@appcoachs.com>
2020-07-22 22:46:25 +0800

    java first lesson

---
+++ Try_Java/FreshJuiceTest.java
@@ -0,0 +1,13 @@
+class FreshJuice {
+    //enum 枚举类型
+    enum FreshJuiceSize{ SMALL, MEDIUM , LARGE }
+    FreshJuiceSize size;
+}
+
+public class FreshJuiceTest {
+    public static void main(String[] args){
+        FreshJuice juice = new FreshJuice();
+        juice.size = FreshJuice.FreshJuiceSize.MEDIUM  ;
+    }
+}
+//.java ->.class->解释执行
